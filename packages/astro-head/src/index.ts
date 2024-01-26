8a41fec849f587f4e55f6e99741dbd051f564a98
Salvador Ferradás <sftpo@pm.me>
2024-01-26 19:15:17 +0100

    refactor: renamed Og to OpenGraph

--- packages/astro-head/src/index.ts
+++ packages/astro-head/src/index.ts
@@ -1,2 +1,2 @@
 export { default as Favicon } from "./components/favicon.astro";
-export { default as Og } from "./components/og.astro";
+export { default as OpenGraph } from "./components/openGraph.astro";
